38966575173ad0f7969ad237e0be7e17a482ecd6
Glitch (minecraft-bot-blaz) <none>
2020-05-20 15:27:29 +0000

    🌺📕 Checkpoint ./index.js:9955294/5076 ./package.json:9955294/413

--- test/externalTests/displayName.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-
-module.exports = () => (bot, done) => {
-  const player = bot.players[bot.username]
-  assert.strictEqual(player.displayName.toString(), bot.username)
-  done()
-}
