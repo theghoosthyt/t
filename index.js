e14de61739709000e6615b430c91f4c6f0d0dc32
Glitch (minecraft-bot-blaz) <none>
2020-05-20 18:08:35 +0000

    📇🎒 Checkpoint ./index.js:9955294/229

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var mineflayer = require('mineflayer');
 var bot = mineflayer.createBot({
-  host: "194.125.251.251", // optional
+  host: "blazserver.aternos.me", // optional
   port: 25565,       // optional
-  username: process.env.EMAIL, // email and password are required only for
-  password: process.env.PASS, // online-mode=true servers
+  username: "Bot", // email and password are required only for
+ 
   version: false                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
 });
 bot.on('chat', function(username, message) {
@@ -11,3 +11,7 @@ bot.on('chat', function(username, message) {
   bot.chat(message);
 });
 bot.on('error', err => console.log(err))
+
+
+
+// password: process.env.PASS,  // online-mode=true servers
