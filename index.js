fb6cbdb0b8914cf767b60db0181d8a0b177e750c
Glitch (minecraft-bot-blaz) <none>
2020-05-20 18:55:34 +0000

    🌋🛰 Checkpoint ./index.js:9955294/3453 ./package.json:9955294/373 ./index.html:9955294/217

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const mineflayer = require('mineflayer');
+//---------------------------------------------------------------------
+// Glitch 24/7
+// Required to let uptime robot waving our bot.
+//---------------------------------------------------------------------
+
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => res.sendFile(__dirname + "/index.html"));
+app.listen(process.env.PORT);
+
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 224000);
+
+// End of Glitch 24/7
+
+const mineflayer = require("mineflayer");
 const bot = mineflayer.createBot({
   host: "blazserver.aternos.me",
-  port: 25565,     
-  username: "Bot", 
-  version: false   
+  port: 25565,
+  username: "Bot",
+  version: false
 });
-bot.on('chat', function(username, message) {
+bot.on("chat", function(username, message) {
   if (username === bot.username) return;
   bot.chat(message);
 });
-bot.on('error', err => console.log(err))
-console.log(`Bot started! Name: ${bot.username}`)
+bot.on("error", err => console.log(err));
+console.log(`Bot started! Name: ${bot.username}`);
