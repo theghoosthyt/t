259f924fcd90d0fec708f0173ca24f095618aaa2
Glitch (minecraft-bot-blaz) <none>
2020-05-24 12:33:21 +0000

    🙈👭 Checkpoint ./index.js:9955294/1237

--- index.js
+++ index.js
@@ -20,58 +20,60 @@ setInterval(() => {
 // End of Glitch 24/7
 
 const mineflayer = require("mineflayer");
-const bot = mineflayer.createBot({
-  host: "blazserver.aternos.me",
-  port: 25565,
-  username: "Bot",
-  version: false
+
+//the config
+const bot = mineflayer.createBot({ 
+  host: "your server address here", //mc.hypixel.net for example
+  port: 25565, //server port (leave it as is unless you know what you're doing!)
+  username: "bot name here", //username only for cracked/offline mode servers, email for premium
+  password: "your password here", //only for premium
+  version: false //version of the server (false = auto detect)
 });
 
-bot.on("move", function() {
+bot.on("move", function() { //triggers when the bot moves
   
-  bot.setControlState("jump", true);
-  setTimeout(() => {
-  bot.setControlState("jump", false);
-  }, 1000);
+  bot.setControlState("jump", true); //continuously jumps
+  setTimeout(() => { //sets a delay
+  bot.setControlState("jump", false); //stops jumping
+  }, 1000); //delay time
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
      
   bot.setControlState("forward", true);
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
         bot.setControlState("forward", false);
             //bot.setControlState("jump", true);
-    }, 500);    
+    }, 500); //delay time
     
-    }, 1000);
+    }, 1000); //delay time
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
 
     bot.setControlState("back", true);
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
           //bot.setControlState("jump", false);
         bot.setControlState("back", false);
-    }, 500); 
+    }, 500); //delay time
      
-   }, 2000);  
+   }, 2000); //delay time
   
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
      
   bot.setControlState("right", true);
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
         bot.setControlState("right", false);
-            //bot.setControlState("jump", true);
-    }, 2000);    
+    }, 2000); //delay time
     
     }, 500);
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
 
     bot.setControlState("left", true);
     
-  setTimeout(() => {
+  setTimeout(() => { //sets a delay
           //bot.setControlState("jump", false);
         bot.setControlState("left", false);
     }, 2000); 
@@ -79,10 +81,6 @@ bot.on("move", function() {
    }, 500);  
   
 });
-bot.on("end", () => {
-  bot();
-});
+bot.on("end", () => bot()); //riggers when the bot leaves/gets kicked and create a new bot from the config
 
-bot.on("error", err => console.log(err));
-
-console.log(`Bot started! Name: ${bot.username}!`);
+bot.on("error", err => console.log(err)); //triggers when there's an error and logs it into console
