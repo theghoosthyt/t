b42001ae5f4ca12467f8be16308c5ed9dc52fb62
Glitch (minecraft-bot-blaz) <none>
2020-05-24 12:57:20 +0000

    👌🍕 Checkpoint ./index.js:9955294/5079

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ setInterval(() => {
 const mineflayer = require("mineflayer");
 
 //the config
-const bot = mineflayer.createBot({ 
+const bot = mineflayer.createBot({
   host: "your server address here", //mc.hypixel.net for example
   port: 25565, //server port (leave it as is unless you know what you're doing!)
   username: "bot name here", //username only for cracked/offline mode servers, email for premium
@@ -30,41 +30,50 @@ const bot = mineflayer.createBot({
   version: false //version of the server (false = auto detect)
 });
 
-bot.on("move", function() { //triggers when the bot moves
-  
+bot.on("move", function() {
+  //triggers when the bot moves
+
   bot.setControlState("jump", true); //continuously jumps
-  setTimeout(() => { //sets a delay
-  bot.setControlState("jump", false); //stops jumping
+  setTimeout(() => {
+    //sets a delay
+    bot.setControlState("jump", false); //stops jumping
   }, 1000); //delay time
-    
-  setTimeout(() => { //sets a delay     
-  bot.setControlState("forward", true); //continuously walks forward    
-  setTimeout(() => { //sets a delay
-        bot.setControlState("forward", false); //stops walking forward
-    }, 500); //delay time    
-    }, 1000); //delay time
-    
-  setTimeout(() => { //sets a delay
-    bot.setControlState("back", true); //continuously walks backwards    
-  setTimeout(() => { //sets a delay
-        bot.setControlState("back", false); //stops walking backwards
-    }, 500); //delay time    
-    }, 2000); //delay time
-  
-  setTimeout(() => { //sets a delay     
-  bot.setControlState("right", true); //continuously walks right    
-  setTimeout(() => { //sets a delay
-        bot.setControlState("right", false); //stops walking right
-    }, 2000); //delay time    
+
+  setTimeout(() => {
+    //sets a delay
+    bot.setControlState("forward", true); //continuously walks forward
+    setTimeout(() => {
+      //sets a delay
+      bot.setControlState("forward", false); //stops walking forward
     }, 500); //delay time
-    
-  setTimeout(() => { //sets a delay
-    bot.setControlState("left", true); //continuously walks lefz    
-  setTimeout(() => { //sets a delay
-        bot.setControlState("left", false); //stops walking left
-   }, 2000); //delay time     
-   }, 500); //delay time
-  
+  }, 1000); //delay time
+
+  setTimeout(() => {
+    //sets a delay
+    bot.setControlState("back", true); //continuously walks backwards
+    setTimeout(() => {
+      //sets a delay
+      bot.setControlState("back", false); //stops walking backwards
+    }, 500); //delay time
+  }, 2000); //delay time
+
+  setTimeout(() => {
+    //sets a delay
+    bot.setControlState("right", true); //continuously walks right
+    setTimeout(() => {
+      //sets a delay
+      bot.setControlState("right", false); //stops walking right
+    }, 2000); //delay time
+  }, 500); //delay time
+
+  setTimeout(() => {
+    //sets a delay
+    bot.setControlState("left", true); //continuously walks lefz
+    setTimeout(() => {
+      //sets a delay
+      bot.setControlState("left", false); //stops walking left
+    }, 2000); //delay time
+  }, 500); //delay time
 });
 bot.on("end", () => bot()); //riggers when the bot leaves/gets kicked and create a new bot from the config
 
