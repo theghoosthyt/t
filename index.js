4851afe797d7ee64a707a03f864bdaee54bae125
Glitch (minecraft-bot-blaz) <none>
2020-08-03 18:29:27 +0000

    📺🌈 Checkpoint ./index.js:20384053/11

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ const mineflayer = require("mineflayer");
 
 //the config
 const bot = mineflayer.createBot({
-  host: "snowworld.ploudos.me", //mc.hypixel.net for example
+  host: "SERVERWASDMCI5.aternos.me", //mc.hypixel.net for example
   port: 25565, //server port (leave it as is unless you know what you're doing!)
-  username: "BOT", //username only for cracked/offline mode servers, email for premium
+  username: "xdxdxd", //username only for cracked/offline mode servers, email for premium
   // password: "CONTRASEÑA", //only for premium
   version: false //version of the server (false = auto detect)
 });
