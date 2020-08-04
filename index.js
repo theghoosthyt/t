ec5a6b89eec63f6ee2f99aae0d9eb6dd78c3af03
theghoosthyt <69209937+theghoosthyt@users.noreply.github.com>
2020-08-04 15:25:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const mineflayer = require("mineflayer");
 const bot = mineflayer.createBot({
   host: "SERVERWASDMCI5.aternos.me", //mc.hypixel.net for example
   port: 25565, //server port (leave it as is unless you know what you're doing!)
-  username: "bot24/7", //username only for cracked/offline mode servers, email for premium
+  username: "Minecraft", //username only for cracked/offline mode servers, email for premium
   // password: "CONTRASEÑA", //only for premium
   version: false //version of the server (false = auto detect)
 });
